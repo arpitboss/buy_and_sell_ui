871d0d5c1f07ef823beef578feca6b1023f1fb4d
Arpit <arpit123boss@gmail.com>
2023-12-25 16:45:10 +0530

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     );
   }
